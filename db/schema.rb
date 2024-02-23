91a571fb54ee496cae80198c34652f94d2fdaba0
tabo-syu <takeshima.nitkc@gmail.com>
2024-02-23 13:08:56 +0000

    fix: 🐛 `rubocop -a`

--- db/schema.rb
+++ db/schema.rb
@@ -10,43 +10,43 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_23_120945) do
+ActiveRecord::Schema[7.1].define(version: 20_240_223_120_945) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "bookmark_has_tags", force: :cascade do |t|
-    t.bigint "bookmark_id", null: false
-    t.bigint "tag_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["bookmark_id"], name: "index_bookmark_has_tags_on_bookmark_id"
-    t.index ["tag_id"], name: "index_bookmark_has_tags_on_tag_id"
+  create_table 'bookmark_has_tags', force: :cascade do |t|
+    t.bigint 'bookmark_id', null: false
+    t.bigint 'tag_id', null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['bookmark_id'], name: 'index_bookmark_has_tags_on_bookmark_id'
+    t.index ['tag_id'], name: 'index_bookmark_has_tags_on_tag_id'
   end
 
-  create_table "bookmarks", force: :cascade do |t|
-    t.text "url"
-    t.text "title"
-    t.text "description"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'bookmarks', force: :cascade do |t|
+    t.text 'url'
+    t.text 'title'
+    t.text 'description'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  create_table "comments", force: :cascade do |t|
-    t.text "body"
-    t.bigint "bookmark_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["bookmark_id"], name: "index_comments_on_bookmark_id"
+  create_table 'comments', force: :cascade do |t|
+    t.text 'body'
+    t.bigint 'bookmark_id', null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['bookmark_id'], name: 'index_comments_on_bookmark_id'
   end
 
-  create_table "tags", force: :cascade do |t|
-    t.string "name"
-    t.string "color"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'tags', force: :cascade do |t|
+    t.string 'name'
+    t.string 'color'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  add_foreign_key "bookmark_has_tags", "bookmarks"
-  add_foreign_key "bookmark_has_tags", "tags"
-  add_foreign_key "comments", "bookmarks"
+  add_foreign_key 'bookmark_has_tags', 'bookmarks'
+  add_foreign_key 'bookmark_has_tags', 'tags'
+  add_foreign_key 'comments', 'bookmarks'
 end
--- test/models/bookmark_has_tag_test.rb
+++ test/models/bookmark_has_tag_test.rb
@@ -1,4 +1,4 @@
-require "test_helper"
+require 'test_helper'
 
 class BookmarkHasTagTest < ActiveSupport::TestCase
   # test "the truth" do
