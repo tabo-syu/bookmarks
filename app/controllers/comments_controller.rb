3cdc9e4ab6929c6480796ce2521aba9b3ed508d2
tabo-syu <takeshima.nitkc@gmail.com>
2024-02-23 17:18:44 +0000

    feat: 🎸 コメントの削除機能の追加

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,16 @@ class CommentsController < ApplicationController
   def create
     @bookmark = Bookmark.find(params[:bookmark_id])
     @comment = @bookmark.comments.create(comment_params)
+
     redirect_to bookmark_path(@bookmark)
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:bookmark_id])
+    @comment = @bookmark.comments.find(params[:id])
+    @comment.destroy
+
+    redirect_to bookmark_path(@bookmark), status: :see_other
   end
 
   private
--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,5 +1,5 @@
 class Bookmark < ApplicationRecord
-  has_many :comments
+  has_many :comments, dependent: :destroy
 
   validates :title, presence: true
   validates :url, url: true
