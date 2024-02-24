383af72c471989d906b2d2ccdb1c8cec53604e2a
tabo-syu <takeshima.nitkc@gmail.com>
2024-02-24 11:55:37 +0000

    feat: 🎸 ログイン機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  before_action :loggedin_user
+
+  private
+
+  def loggedin_user
+    @user = current_user
+  end
 end
