a2a5c31d08c00d33462d253c62b10f9376c3bea0
tabo-syu <takeshima.nitkc@gmail.com>
2024-03-20 12:52:49 +0000

    feat: 🎸 ホーム画面にタグ一覧を追加

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @bookmarks = Bookmark.includes(%i[tags user])
+    @bookmarks = Bookmark.order(created_at: :desc).preload(%i[tags user])
+    @tags = Tag.all
   end
 end
